14b9ff41fa42bb93a7ec0a0e9b996a7ce8371c62
gmnic0 <107402562+gmnic0@users.noreply.github.com>
2022-08-22 20:06:07 -0300

    /tadproject

---
+++ TAD project/CalculadoraComplexos.py
@@ -0,0 +1,17 @@
+from TAD import *
+
+
+#print(f'partes reais: a:{a.real}, b:{b.real}, c:{c.real}')
+#print(f'partes imaginarias: a:{a.imag}, b:{b.imag}, c:{c.imag}')
+print(f'soma:{soma()}\nsubtração:{sub()}\nmultiplicacao:{mult()}\ndivisão:{div()}')
+print(f'          partes reais:\n'
+      f'Soma:          {soma().real}\n'
+      f'subtração:     {sub().real}\n'
+      f'multiplicacao: {mult().real}\n'
+      f'divisão:       {div().real}\n')
+
+print(f'           partes imaginarias:\n'
+      f'Soma:          {soma().imag}\n'
+      f'subtração:     {sub().imag}\n'
+      f'multiplicacao: {mult().imag}\n'
+      f'divisão:       {div().imag}\n')
---
+++ TAD project/TAD.py
@@ -0,0 +1,20 @@
+a = complex(1, 2)
+b = complex(3, 4)
+c = complex(5, 6)
+
+def soma():
+  soma = a + b + c
+  return soma
+
+
+def sub():
+   sub = a - b - c
+   return sub
+
+def mult():
+   mult = a * b * c
+   return mult
+
+def div():
+   div = (a/b)/c
+   return div
